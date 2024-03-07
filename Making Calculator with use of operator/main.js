d354966fbca634471c95f56b4d1f9bff8fb941d0
hamza2171002 <muhammadhamza9191@gmail.com>
2024-03-07 08:23:10 +0500

    first commit

---
+++ Making Calculator with use of operator/main.js
@@ -0,0 +1,22 @@
+// Making Calculator with use of operator.
+//Making veriables.
+var num1 = 8;
+var num2 = 4;
+//addition
+var add = (num1 + num2);
+console.log(add);
+//subtraction
+var sub = (num1 - num2);
+console.log(sub);
+//multiplication
+var mul = (num1 * num2);
+console.log(mul);
+//division
+var div = (num1 / num2);
+console.log(div);
+//modulus
+var mod = (num1 % num2);
+console.log(mod);
+//exponentiation
+var exp = (Math.pow(num1, num2));
+console.log(exp);
---
+++ Making Calculator with use of operator/main.ts
@@ -0,0 +1,23 @@
+// Making Calculator with use of operator.
+//Making veriables.
+let num1: number = 8
+let num2: number = 4
+
+//addition
+let add = (num1 + num2)
+console.log(add)
+//subtraction
+let sub = (num1 - num2)
+console.log(sub)
+//multiplication
+let mul = (num1 * num2)
+console.log(mul)
+//division
+let div = (num1 / num2)
+console.log(div)
+//modulus
+let mod = (num1 % num2)
+console.log(mod)
+//exponentiation
+let exp = (num1**num2)
+console.log(exp)
